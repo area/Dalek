c1353936719b196611e0a70d843727a9c771012a
Phil Richman <practual@protonmail.com>
2025-03-26 22:26:58 +0000

    New audio library to monitor output

--- audio.py
+++ audio.py
@@ -1,53 +1,10 @@
-import asyncio
-
-import pyaudio
-
-
-def list_devices():
-    p = pyaudio.PyAudio()
-    host_api_info = p.get_host_api_info_by_type(pyaudio.paALSA)
-    host_api_index = host_api_info["index"]
-    for d in range(host_api_info["deviceCount"]):
-        device_info = p.get_device_info_by_host_api_device_index(host_api_index, d)
-        print(device_info)
-    p.terminate()
+from pulsectl_asyncio import PulseAsync
 
 
 async def monitor_audio_output(ears):
-    def stream_callback(in_data, frame_count, time_info, status):
-        # print(sum(in_data) / frame_count)
-        # 4 bytes per frame? 2 channels * 2 byte width?
-        print(sum(in_data) / frame_count)
-        if sum(in_data) / frame_count > 200:
-            ears.flash()
-        return None, pyaudio.paContinue
-
-    p = pyaudio.PyAudio()
-
-    host_api_info = p.get_host_api_info_by_type(pyaudio.paALSA)
-    host_api_index = host_api_info["index"]
-    device_index = None
-    sample_rate = None
-    for d in range(host_api_info["deviceCount"]):
-        device_info = p.get_device_info_by_host_api_device_index(host_api_index, d)
-        if "Loopback: PCM (hw:1,1)" in device_info["name"]:
-            device_index = device_info["index"]
-            sample_rate = device_info["defaultSampleRate"]
-            break
-
-    stream = p.open(
-        rate=int(44100),
-        channels=2,
-        format=p.get_format_from_width(2),
-        input=True,
-        input_device_index=2,
-        stream_callback=stream_callback,
-    )
-
-    while True:
-        try:
-            await asyncio.sleep(10)
-        except asyncio.CancelledError:
-            stream.close()
-            p.terminate()
-            raise
+    async with PulseAsync("dalek_ears") as pulse:
+        server_info = await pulse.server_info()
+        default_sink_info = await pulse.get_sink_by_name(server_info.default_sink_name)
+        async for level in pulse.subscribe_peak_sample(default_sink_info.monitor_source_name):
+            if level > 0.5:
+                ears.flash()
--- ears.py
+++ ears.py
@@ -1,6 +1,17 @@
+import asyncio
+
+
 class Ears:
     def __init__(self, pin):
-        self.pin = pin
+        self._pin = pin
+        self._flash_task = None
 
     def flash(self):
-        self.pin.on()
+        self._pin.on()
+        if self._flash_task:
+            self._flash_task.cancel()
+        self._flash_task = asyncio.create_task(self._off())
+
+    async def _off(self):
+        await asyncio.sleep(0.5)
+        self._pin.off()
