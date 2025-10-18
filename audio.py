35c85f5c933b17fb4068d89a05d0add6625a1b82
Davros <davros@skaro.com>
2025-10-18 14:17:23 +0100

    Wait until Pulse server is available

--- audio.py
+++ audio.py
@@ -1,4 +1,8 @@
+import asyncio
+import os
+
 from pulsectl_asyncio import PulseAsync
+from pulsectl_asyncio.pulsectl_async import PulseError
 
 from ears import Ears
 
@@ -6,8 +10,26 @@ EAR_TRIGGER_THRESHOLD = 0.66
 
 
 async def monitor_audio_output(ears: Ears):
+    # Explicitly set some required env variables that aren't
+    # automatically present when starting via cron.
+    os.environ["XDG_RUNTIME_DIR"] = f"/run/user/{os.getuid()}"
     print("Starting audio output monitoring")
-    async with PulseAsync("dalek_ears") as pulse:
+    pa = PulseAsync("dalek_ears")
+    error_count = 0
+    while True:
+        try:
+            pulse = await pa.__aenter__()
+        except PulseError:
+            # PulseAudio server not up and running yet.
+            error_count += 1
+            if error_count > 5:
+                print("Error connecting to PulseAudio too many times. Raising...")
+                raise
+            print("Sleep until PulseAudio server available...")
+            await asyncio.sleep(30)
+        else:
+            break
+    try:
         server_info = await pulse.server_info()
         print("Got server info", server_info)
         default_sink_info = await pulse.get_sink_by_name(server_info.default_sink_name)
@@ -16,3 +38,5 @@ async def monitor_audio_output(ears: Ears):
             if level > EAR_TRIGGER_THRESHOLD:
                 print(f"Audio level {level} is above threshold, flashing ears...")
                 ears.flash()
+    finally:
+        pa.__aexit__(None, None, None)
