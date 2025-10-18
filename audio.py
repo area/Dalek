d74bbe0421c6c279e1d1904d9726bc0f1eb2121a
Davros <davros@skaro.com>
2025-10-18 12:42:21 +0100

    Ear trigger levels

--- audio.py
+++ audio.py
@@ -1,10 +1,18 @@
 from pulsectl_asyncio import PulseAsync
 
+from ears import Ears
 
-async def monitor_audio_output(ears):
+EAR_TRIGGER_THRESHOLD = 0.66
+
+
+async def monitor_audio_output(ears: Ears):
+    print("Starting audio output monitoring")
     async with PulseAsync("dalek_ears") as pulse:
         server_info = await pulse.server_info()
+        print("Got server info", server_info)
         default_sink_info = await pulse.get_sink_by_name(server_info.default_sink_name)
+        print("Got default sink", default_sink_info)
         async for level in pulse.subscribe_peak_sample(default_sink_info.monitor_source_name):
-            if level > 0.5:
+            if level > EAR_TRIGGER_THRESHOLD:
+                print(f"Audio level {level} is above threshold, flashing ears...")
                 ears.flash()
--- ears.py
+++ ears.py
@@ -1,17 +1,22 @@
 import asyncio
 
+from gpiozero import LED
+
+PERSIST_TIME_S = 0.4
+
 
 class Ears:
-    def __init__(self, pin):
+    def __init__(self, pin: LED):
         self._pin = pin
         self._flash_task = None
 
-    def flash(self):
+    def flash(self) -> None:
+        if self._flash_task and not self._flash_task.done():
+            return
         self._pin.on()
-        if self._flash_task:
-            self._flash_task.cancel()
+
         self._flash_task = asyncio.create_task(self._off())
 
     async def _off(self):
-        await asyncio.sleep(0.05)
+        await asyncio.sleep(PERSIST_TIME_S)
         self._pin.off()
